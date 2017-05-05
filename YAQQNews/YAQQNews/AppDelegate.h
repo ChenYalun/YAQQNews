4e3ffa02ad8774c0916a3726b513ac50939ada8b
chenyalun <iyalun@outlook.com>
2017-05-05 22:32:04 +0800

    初始化项目

---
+++ YAQQNews/YAQQNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YAQQNews
+//
+//  Created by 陈亚伦 on 2017/5/5.
+//  Copyright © 2017年 陈亚伦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YAQQNews/YAQQNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YAQQNews
+//
+//  Created by 陈亚伦 on 2017/5/5.
+//  Copyright © 2017年 陈亚伦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
